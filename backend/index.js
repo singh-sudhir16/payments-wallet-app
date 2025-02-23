d374a401040a2b9ea71d28a35911f5a063dc93ba
Sudhir Singh <mail.sudhir.singh14@gmail.com>
2025-02-23 23:20:43 +0530

    add index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const mainRouter = require('./routes/index');
 app.use("/api/v1" , mainRouter);
 
 // app.use("/api/v2" , mainRo   uter);
-
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'backend ekdam changa hai ji' });
+  });
+  
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

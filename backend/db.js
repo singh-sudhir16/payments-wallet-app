d7a513b314741e22389dfe19785a65d7605f61ba
Sudhir Singh <mail.sudhir.singh14@gmail.com>
2025-02-23 22:51:23 +0530

    final changes made in backend

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,7 @@
 require('dotenv').config(); 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI)
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
     username: {
